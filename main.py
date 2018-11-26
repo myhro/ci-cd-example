bb2fdb03dcb5509eb78b22a68705e9cffe8f1091
Tiago Ilieve <tiago.myhro@gmail.com>
2018-11-26 16:45:48 +0100

    Fix lint errors

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import unittest
 
+
 def my_sum(x, y):
     return x + y
+
 
 class TestMySum(unittest.TestCase):
     def test_two_plus_two(self):
         result = my_sum(2, 2)
         self.assertEqual(result, 4)
+
 
 unittest.main()
